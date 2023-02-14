7c5b94f280b7fb11f996f41dc21d622a1602043f
Marcus Sandi <tradedomarcus@gmail.com>
2023-02-14 14:11:52 -0300

    refactor: alter temporary extension

---
+++ app/layout.jsx
@@ -0,0 +1,21 @@
+import Nav from './auth/Nav'
+import './globals.css'
+import { Roboto } from '@next/font/google'
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ['400', '700'],
+  variable: '--font-roboto'
+})
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-br">
+      <head />
+      <body className={`mx-4 md:mx-48 xl-96 ${roboto.variable} bg-slate-200`}> 
+        <Nav/>
+        {children}
+        </body>
+    </html>
+  )
+}
