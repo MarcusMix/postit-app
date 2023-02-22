d4f238a4254fd24379800ced00e8d893a126825f
Marcus Sandi <tradedomarcus@gmail.com>
2023-02-22 14:46:25 -0300

    feat: add post

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,35 @@
+import axios from "axios";
 import AddPost from "./components/AddPost";
+import { useQuery } from "react-query";
+import Post from "./components/Post";
+import { PostType } from "./types/Posts";
+
+
+const allPosts = async () => {
+  const response = await axios.get('/api/get/getPosts')
+  return response.data
+}
 
 export default function Home() {
+  const { data, error, isLoading } = useQuery<PostType[]>({
+    queryFn: allPosts,
+    queryKey: ['posts']
+  })
+  if(error) return error
+  if(isLoading) return "Loading..."
   return (
     <main >
-      <h1 className="py-200">oi</h1>
       <AddPost/>
+      {data?.map((post) => (
+          <Post  
+            Comment={post.Comment}
+            key={post.id} 
+            name={post.user.name} 
+            avatar={post.user.image}
+            postTitle={post.title}
+            id={post.id}
+          />
+        ))}
     </main>
   )
 }
